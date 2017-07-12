52c965a6ebea62e236eeb830145ddb06cde53fae
Boyoon Choi <boyoonchoi@Boyoons-MacBook-Pro.local>
2017-07-11 21:35:22 -0400

    added printing

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.listen(3000, function(){
 	console.log("server listening")
 })
 
+app.use(function(req, res, next){
+	console.log(req.method, req.originalUrl)
+})
+
 app.get('/', function(req, res, next){
 	res.send('Hey!')
 })
