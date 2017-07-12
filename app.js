1912803b209ae65b4e6e244e877fda4c037c1c28
Boyoon Choi <boyoonchoi@Boyoons-MacBook-Pro.local>
2017-07-11 21:22:31 -0400

    we got

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const express = require( 'express' );
 const app = express();
 
-// app.listen()
+app.listen(3000, function(){
+	console.log("server listening")
+})
+
+app.get('/', function(req, res, next){
+	res.send('Hey!')
+})
