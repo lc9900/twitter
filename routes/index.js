a7b72aed3f1b5161449ea56b2b5c461f96f65cc8
Boyoon Choi <boyoonchoi@119.78.119.52.naturalwireless.com>
2017-07-12 22:14:10 -0400

    can look at users tweets

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,15 @@ const router = express.Router();
 
 const tweetBank = require('../tweetBank');
 
-// router.get('/stylesheets/style.css', function(req, res, next){
-// 	res.sendFile(path.join(__dirname +'/../public/stylesheets/style.css'))
-// })
-
 router.get('/', function (req, res) {
   let tweets = tweetBank.list();
   res.render( 'index', { tweets: tweets } );
+});
+
+router.get('/users/:name', function(req, res) {
+  var name = req.params.name;
+  var list = tweetBank.find( {name: name} );
+  res.render( 'index', { tweets: list } );
 });
 
 module.exports = router;
