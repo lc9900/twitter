39218ff77c43e15be3fc2ff3672a0d7e7b02b1fe
Boyoon Choi <boyoonchoi@119.78.119.52.naturalwireless.com>
2017-07-12 21:39:49 -0400

    routed to css

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const path = require('path');
 const router = express.Router();
 
 const tweetBank = require('../tweetBank');
+
+router.get('/stylesheets/style.css', function(req, res, next){
+	res.sendFile(path.join(__dirname +'/../public/stylesheets/style.css'))
+})
 
 router.get('/', function (req, res) {
   let tweets = tweetBank.list();
